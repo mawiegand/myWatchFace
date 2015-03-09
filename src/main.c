6b83d98afb3519d52c7b1e612382c364e0ceb748
Marcel Wiegand <marcel@bluepanel.org>
2015-03-09 18:44:45 +0100

    Add background image and change font colour

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   // Get a tm structure
@@ -26,17 +28,27 @@ static void update_time() {
 }
 
 static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+  #ifdef PBL_COLOR
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND_BLUE);
+  #else
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  #endif
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+  
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(5, 52, 139, 50));
+  s_time_layer = text_layer_create(GRect(5, 59, 139, 40));
   text_layer_set_background_color(s_time_layer, GColorClear);
   #ifdef PBL_COLOR
-    text_layer_set_text_color(s_time_layer, GColorWhite);
+    text_layer_set_text_color(s_time_layer, GColorDarkGray);
   #else
     text_layer_set_text_color(s_time_layer, GColorBlack);
   #endif
   
   // Create GFont
-  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NASALIZATION_48));
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_NASALIZATION_38));
 
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
@@ -57,6 +69,12 @@ static void main_window_unload(Window *window) {
   
   // Unload GFont
   fonts_unload_custom_font(s_time_font);
+  
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+  
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -73,11 +91,11 @@ static void init() {
     .unload = main_window_unload
   });
   
-  #ifdef PBL_COLOR
+  /*#ifdef PBL_COLOR
     window_set_background_color(s_main_window, GColorBlueMoon);
   #else
     window_set_background_color(s_main_window, GColorBlack);
-  #endif
+  #endif*/
   
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
